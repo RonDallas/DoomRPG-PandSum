5f49a7f1aed0fdc683c55cf01248923b6da8f515
Sumwunn <0x77VA@gmail.com>
2024-12-19 14:03:01 -0500

    CorruptionCards v6.3 Patch (v6.2 is retained), New Medical sprites from Wickez (https://github.com/Sumwunn/DoomRPG/issues/98), Updated GDCC
    Need to look at LegenDoom next.

--- 0compile.bat
+++ 0compile.bat
@@ -1,3 +1,3 @@
-Utilities\astyle\astyle.exe --style=allman --recursive DoomRPG/scripts/*.c,*.h DoomRPG/zscript/*.zs
+Utilities\astyle\astyle.exe --style=allman --recursive DoomRPG/scripts/*.c,*.h DoomRPG/zscript/*.zs DoomRPG-CorruptionCards/zscript/*.zs DoomRPG-CorruptionCards/zscript/corruptioncards/*.zscript
 Utilities\python\python.exe compile.py 
 del IR\*.obj
